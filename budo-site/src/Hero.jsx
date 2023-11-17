08ece29dd7ea9e637d70a61ea389fc6ff4027353
BramV-2158806 <99867493+BramV-2158806@users.noreply.github.com>
2023-11-17 23:21:02 +0100

    Changed the title and the logo

--- budo-site/src/Hero.jsx
+++ budo-site/src/Hero.jsx
@@ -3,7 +3,7 @@ import './Hero.css'
 export default function Hero() {
 
   return (
-      <div className="w-screen bg-white">
+      <div className="w-screen h-screen bg-white">
         <div className="relative isolate px-6 lg:px-8">
           <div
             className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
@@ -18,15 +18,6 @@ export default function Hero() {
             />
           </div>
           <div className="md:container mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-            <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-              <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-                Announcing our next round of funding.{' '}
-                <a href="#" className="font-semibold text-indigo-600">
-                  <span className="absolute inset-0" aria-hidden="true" />
-                  Read more <span aria-hidden="true">&rarr;</span>
-                </a>
-              </div>
-            </div>
             <div className="text-center">
               <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
                 Tailor made software solutions.
